fa2a7074500c2bece4dd033b84df1ccf2026df68
DinaMutan <sergiofusha05@gmail.com>
2026-01-10 08:52:37 +0100

    update plan

--- KiberONE_Manager/01_Classes/Saturday_0900/Resources/Flappy_Bird/code/snapshot_2025-12-20.py
+++ KiberONE_Manager/01_Classes/Saturday_0900/Resources/Flappy_Bird/code/snapshot_2025-12-20.py
@@ -1,6 +1,9 @@
 import pgzrun
 import random
 
+from pgzero.actor import Actor
+from pgzero.game import screen
+
 # --- CONFIGURATION ---
 TITLE = "Flappy Bird - Modular Lesson"
 WIDTH = 288
@@ -33,21 +36,21 @@ game_active = True
 def draw():
     screen.clear()
     bg.draw()
-    
+
     # Draw pipes (under ground)
     pipe_top.draw()
     pipe_bottom.draw()
-    
+
     ground1.draw()
     ground2.draw()
     bird.draw()
-    
-    if not game_active:
-        screen.draw.text("GAME OVER", center=(WIDTH/2, HEIGHT/2), fontsize=50, color="orange", shadow=(1,1))
+
+    #if not game_active:
+    #    screen.draw.text("GAME OVER", center=(WIDTH/2, HEIGHT/2), fontsize=50, color="orange", shadow=(1,1))
 
 def update():
     global game_active
-    
+
     if not game_active:
         return
 
@@ -58,7 +61,7 @@ def update():
     # PART 1: Infinite Ground Scroll
     ground1.x -= SPEED
     ground2.x -= SPEED
-    
+
     if ground1.right < 0:
         ground1.left = ground2.right
     if ground2.right < 0:
@@ -67,22 +70,22 @@ def update():
     # PART 2: Simple Pipe Loop
     pipe_top.x -= SPEED
     pipe_bottom.x -= SPEED
-    
+
     if pipe_top.right < 0:
         pipe_top.left = WIDTH
         pipe_bottom.left = WIDTH
-        
+
         # PART 3: Adding Randomness
-        gap_y = random.randint(150, 350)
-        pipe_top.y = gap_y - 60
-        pipe_bottom.y = gap_y + 60
+        #gap_y = random.randint(150, 350)
+        #pipe_top.y = gap_y - 60
+        #pipe_bottom.y = gap_y + 60
 
     # PART 4: Collision Logic
-    if bird.colliderect(pipe_top) or bird.colliderect(pipe_bottom):
-        game_active = False
-        
-    if bird.top < 0 or bird.bottom > HEIGHT - 50: # -50 is ground level
-        game_active = False
+    #if bird.colliderect(pipe_top) or bird.colliderect(pipe_bottom):
+    #    game_active = False
+    #
+    #if bird.top < 0 or bird.bottom > HEIGHT - 50: # -50 is ground level
+    #    game_active = False
 
 def on_key_down(key):
     if key == keys.SPACE and game_active:
